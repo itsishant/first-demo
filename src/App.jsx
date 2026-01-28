d99c15440fdac8cfe5225ac9e0a2f905e261f2da
itsishant <modakmania@gmail.com>
2026-01-28 16:01:50 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+function App() {
+  const handleClick = () => {
+    alert("helllo world")
+  }
+  return (
+    <div className="bg-blue-600 min-h-screen">
+      <div className="font-extrabold flex">Sign up</div>
+      <div className="space-x-4">
+        <input type="text" placeholder="Email" className="rounded-lg"></input>
+        <input
+          type="text"
+          placeholder="Password"
+          className="rounded-lg"
+        ></input>
+        <button className="p-2 bg-green-600 rounded-lg " onClick={handleClick}>
+          Sign up
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
